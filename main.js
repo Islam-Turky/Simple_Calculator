cdd9e13e5dcc0a3311c036f779705a573f7333d7
Islam Turky <107361423+Islam-Turky@users.noreply.github.com>
2023-07-24 12:46:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -61,80 +61,17 @@ inputs.forEach((input) => {
             calculator.dataset.previousKey = 'number';
         }
 
-        // if( action === 'sum' ||
-        //     action === 'multi' ||
-        //     action === 'div' ||
-        //     action === 'minus'
-        // ){
-        //     const firstValue = calculator.dataset.firstValue;
-        //     const operator = calculator.dataset.operator;
-        //     const secondValue = displayedNum;
-
-        //     // if (
-        //     //     firstValue &&
-        //     //     operator &&
-        //     //     previousKeyType !== 'operator'
-        //     // ) {
-        //     //     const calcValue = calculate(firstValue, operator, secondValue);
-        //     //     screenResult.textContent = calcValue;
-                
-        //     // // Update calculated value as firstValue
-        //     //     calculator.dataset.firstValue = calcValue;
-        //     // } else {
-        //     //     // If there are no calculations, set displayedNum as the firstValue
-        //     //     calculator.dataset.firstValue = displayedNum;
-        //     // }
-
-        //     if (
-        //         firstValue &&
-        //         operator &&
-        //         previousKeyType !== 'operator' &&
-        //         previousKeyType !== 'calculate'
-        //     ) {
-        //         const calcValue = calculate(firstValue, operator, secondValue);
-        //         screenResult.textContent = calcValue;
-        //         calculator.dataset.firstValue = calcValue;
-        //     } else {
-        //         calculator.dataset.firstValue = displayedNum;
-        //     }
-            
-        //     key.classList.add('is-depressed');
-        //     calculator.dataset.previousKeyType = 'operator';
-        //     calculator.dataset.operator = action;
-        // }
-
         if (action === 'clear'){
             screenResult.textContent = '';
         }
-            
-        //     screenResult.textContent = 0;
-        //     calculator.dataset.previousKeyType = 'clear';
-        // }
-
 
         if (action === 'equal'){
             screenResult.textContent = eval(displayedNum);
         }
-            
-        //     // Set modValue attribute
-        //     calculator.dataset.modValue = secondValue;
-        //     calculator.dataset.previousKeyType = 'calculate';
-        // }
 
-        // if (action === 'decimal') {
-        //     if (!displayedNum.includes('.')) {
-        //         screenResult.textContent = displayedNum + '.';
-        //     } else if (
-        //         previousKeyType === 'operator' ||
-        //         previousKeyType === 'calculate'
-        //     ) {
-        //         screenResult.textContent = '0.';
-        //     }
-            
-        //     calculator.dataset.previousKeyType = 'decimal';
-        // }
-
-        // Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
+        if (action === 'delete'){
+            screenResult.textContent = displayedNum.slice(0, -1);
+        }
     });
 });
 
